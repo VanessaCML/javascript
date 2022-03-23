ba6aecfdf9601c50866a052ef01bdd7d4d00db94
Vanessa Matos <vanessacmatos@outlook.com>
2022-03-23 21:21:46 +0800

    exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80.5
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if (vel>80){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Use cinto de segurança.`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'China'
+console.log(`Vivendo no país ${país}`)
+if (país == 'Brasil'){
+    console.log(`Brasileiro`)
+} else{
+    console.log(`Estrangeiro`)
+}
